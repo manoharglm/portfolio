03787a2f501ac4ce504dc4b51436c160c444832f
manoharglm <manohargunduboina@gmail.com>
2020-04-14 22:58:15 +0530

    Updates

---
+++ precache-manifest.70850da65b9cda06da474352d7cfd221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2353a2ed1670dc743213c1e8c7fcbab8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc234b1a758b125cc89c",
+    "url": "/static/css/main.af63b3d7.chunk.css"
+  },
+  {
+    "revision": "f8cc92987967a502f46f",
+    "url": "/static/js/2.3aa241c4.chunk.js"
+  },
+  {
+    "revision": "640aa39c32c9bb605adf655ed1141834",
+    "url": "/static/js/2.3aa241c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc234b1a758b125cc89c",
+    "url": "/static/js/main.d212f216.chunk.js"
+  },
+  {
+    "revision": "b0d7bc65b4b32fe4306a",
+    "url": "/static/js/runtime-main.1e4818d4.js"
+  },
+  {
+    "revision": "5bf2394fabe5e0e60a1c5627eefdd7b3",
+    "url": "/static/media/manohar.5bf2394f.jpg"
+  }
+]);
