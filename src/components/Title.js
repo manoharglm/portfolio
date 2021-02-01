174652109d710891508317bc7e533aeda1b84a1d
Manohar Gunduboina <34521899+thecodeabides@users.noreply.github.com>
2021-02-01 21:41:03 +0530

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 import { IconButton } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import InstagramIcon from "@material-ui/icons/Instagram";
+import InstagramIcon from "@material-ui/icons/Twitter";
 import useStyles from "../styles";
 import Skeleton from 'react-loading-skeleton';
 
